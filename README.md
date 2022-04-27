77782f556ca3031dd5a03a7f14644d392c5429c8
NYNEX SERVER 5 <65372786+CodeSalvageON@users.noreply.github.com>
2022-04-27 18:28:09 +0000

    Card selection (new crusade)

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 A political enigma.
 
 ## Credits
-All images are under Public Domain.
+All images are under Public Domain (in the United States, given that all of their artists died well over 100 years ago).
 * The map of the Holy Roman Empire is considered public domain in the United States; its creator and artist being long dead.
 * Marcia trionfale (Hallmayer), also known as the early anthem of the Papal States, is considered public domain in the United States, its composer having died over 100 years ago.
--- holyrome/v1/btn.js
+++ holyrome/v1/btn.js
@@ -1,6 +1,8 @@
 const newCrusade = document.getElementById("new");
 const contCrusade = document.getElementById("cont");
 
+const cardsTable = document.getElementsByClassName("cards-table-inline");
+
 $(".scroll").click(function () {
   if (isIntDisabled === true) {
     return false;
@@ -10,4 +12,102 @@ $(".scroll").click(function () {
 newCrusade.onclick = function () {
   austriaAnthem.play();
   fadeToIntro();
+}
+
+const card1 = document.getElementById("card1");
+const card2 = document.getElementById("card2");
+const card3 = document.getElementById("card3");
+const card4 = document.getElementById("card4");
+const card5 = document.getElementById("card5");
+const card6 = document.getElementById("card6");
+
+let card1Click = 0;
+let card2Click = 0;
+let card3Click = 0;
+
+let card1Selec = "";
+let card2Selec = "";
+let card3Selec = "";
+
+const chooseCards = document.getElementById("choose-cards");
+
+function cardSelectFunc (which) {
+  if (which === card1Selec || which === card2Selec || which === card3Selec) {
+    card1Selec = "";
+    card2Selec = "";
+    card3Selec = "";
+    card1Click = 0;
+    card2Click = 0;
+    card3Click = 0;
+
+    for (i = 0; i < cardsTable.length; i++) {
+      cardsTable[i].style.border = "2px solid brown";
+    }
+    
+    return false;
+  }
+  
+  if (card1Click === 0) {
+    card1Click = 1;
+    card1Selec = which;
+    document.getElementById(which).style.border = "2px solid black";
+  }
+
+  else {
+    if (card2Click === 0) {
+      card2Click = 1;
+      card2Selec = which;
+      document.getElementById(which).style.border = "2px solid black";
+    }
+
+    else {
+      if (card3Click === 0) {
+        card3Click = 1;
+        card3Selec = which;
+        document.getElementById(which).style.border = "2px solid black";
+      }
+
+      else {
+        document.getElementById(which).style.border = "2px solid black";
+        document.getElementById(card3Selec).style.border = "2px solid brown";
+        card3Selec = which;
+      }
+    }
+  }
+}
+
+card1.onclick = function () {
+  cardSelectFunc("card1");
+}
+
+card2.onclick = function () {
+  cardSelectFunc("card2");
+}
+
+card3.onclick = function () {
+  cardSelectFunc("card3");
+}
+
+card4.onclick = function () {
+  cardSelectFunc("card4");
+}
+
+card5.onclick = function () {
+  cardSelectFunc("card5");
+}
+
+card6.onclick = function () {
+  cardSelectFunc("card6");
+}
+
+let chooseErr = document.getElementById("choose-err");
+
+chooseCards.onclick = function () {
+  if (card1Click === 1 && card2Click === 1 && card3Click === 1) {
+    
+  }
+
+  else {
+    chooseErr.innerText = "Please choose three cards first.";
+  }
 }
